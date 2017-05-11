c716798827777e3ec0cd1fe683b5d1bd29aaf180
Andrew Kemp <andrew.kemp@bynd.com>
2017-05-11 15:51:47 -0400

    ES6 Upgrade

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
-var Promise = require('promise');
-var summarize = require('summarize');
-var summary = require('node-summary');
-var superagent = require('superagent');
-var unfluff = require('unfluff');
+const summarize = require('summarize')
+const summary = require('node-summary')
+const superagent = require('superagent')
+const unfluff = require('unfluff')
 
-var summarizeP = Promise.denodeify(summary.summarize);
-
-module.exports.getPage = function (uri) {
-  'use strict';
-
-  return new Promise(function (resolve, reject) {
-    superagentGetP(uri).then(function (data) {
-      var text = data.text;
-      var pageContent = unfluff(text);
-      pageContent.raw = text;
-      pageContent.stats = summarize(text);
-      summarizeP(pageContent.title, pageContent.text).then(function (res) {
-        pageContent.summary = res;
-        resolve(pageContent);
-      }, reject);
-    }, reject);
-  });
-};
-
-function superagentGetP(uri) {
-  'use strict';
-
-  return new Promise(function (resolve, reject) {
-    superagent.get(uri).end(function (err, res) {
-      if (err) {
-        return reject(err);
-      }
-      resolve(res);
-    });
-  });
+const summarizeArticle = (title, story) => {
+    return new Promise((Resolve, Reject) => {
+        summary.summarize(title, story, (err, summary) => {
+            if (err) return Reject(err)
+            summary = summary.split('/n').pop().replace('/n', '') // remove title from summary
+            return Resolve(summary)
+        })
+    })
 }
+
+const superagentGetP = (uri) => {
+    return new Promise((Resolve, Reject) => {
+        superagent.get(uri)
+            .end((err, res) => {
+                if (err) return Reject(err)
+                return Resolve(res)
+            })
+    })
+}
+
+module.exports.getPage = (uri) => {
+    return new Promise((Resolve, Reject) => {
+        superagentGetP(uri)
+            .then(data => {
+                const text = data.text
+                let pageContent = unfluff(text)
+                pageContent.raw = text
+                pageContent.stats = summarize(text)
+                summarizeArticle(pageContent.title, pageContent.text)
+                    .then((res) => {
+                        pageContent.summary = res
+                        Resolve(pageContent)
+                    }, Reject)
+            }, Reject)
+    })
+}
+
