3f3d530a1eaf460bfdf2d39d98f3ac6cefac3541
Andrew Kemp <andrew.kemp@bynd.com>
2017-05-17 11:57:00 -0400

    Updating test suite

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ const summarizer = require('../index')
 const url = 'http://nodejs.org/api/documentation.html'
 
 
-describe('Test Summary', function () {
+describe('Test Full Summary', function () {
     let summary;
     before(function () {
         summary = summarizer(url)
@@ -16,6 +16,27 @@ describe('Test Summary', function () {
     it('Returned data is an object', function () {
         summary.then(results => {
             assert.isObject(results, 'Results of summary is object')
+        })
+    })
+})
+
+describe('Test Full Summary w/ Raw', function () {
+    let summary;
+    before(function () {
+        summary = summarizer(url, { includeRaw: true })
+    })
+    it('Summarize resolved without error', function () {
+        return summary
+    })
+    it('Returned data is an object', function () {
+        summary.then(results => {
+            assert.isObject(results, 'Results of summary is object')
+        })
+    })
+    it('Returned data has raw property', function () {
+        summary.then(results => {
+            assert.property(results, 'raw')
+            assert.isString(results.raw)
         })
     })
 })
